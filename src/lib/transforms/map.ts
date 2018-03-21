9e47a001b55aca5c21f84112e0533241695b380d
Johannes Karow <johannes.karow@gmail.com>
2018-03-21 11:14:12 +0100

    Update map.ts

--- src/lib/transforms/map.ts
+++ src/lib/transforms/map.ts
@@ -2,6 +2,6 @@ import { TransformObjectData } from './transform_object_data'
 
 export class Map<T, S> extends TransformObjectData<T, S> {
   constructor(handler: (src: T) => S) {
-    super(handler) // the deault handler is actually a map function
+    super(handler) // the default handler is actually a map function
   }
 }
